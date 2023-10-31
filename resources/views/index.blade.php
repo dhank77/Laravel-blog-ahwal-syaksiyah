decb4f3a415e12a2e9cde46593e115af221ce6e4
Hamdani Ilham <d41113512@gmail.com>
2023-10-31 08:53:36 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -108,3 +108,11 @@
 </div><!-- end row-->
 
 @endsection
+@section('script')
+<!-- apexcharts -->
+<script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ URL::asset('/assets/libs/admin-resources/admin-resources.min.js') }}"></script>
+
+<!-- dashboard init -->
+<script src="{{ URL::asset('/assets/js/pages/dashboard.init.js') }}"></script>
+@endsection
