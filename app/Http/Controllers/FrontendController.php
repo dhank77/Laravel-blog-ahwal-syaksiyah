d05b23431fc8fbc6b29b377580d5c2a476e8e753
Hamdani Ilham <d41113512@gmail.com>
2024-02-20 09:42:33 +0800

    psg4

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -197,8 +197,14 @@ class FrontendController extends Controller
 
         $paramsUrl = url("new-result" . date("ymdhis") . ".docx");
         set_time_limit(0); 
-        $link = "https://psg4-word-view.officeapps.live.com/wv/WordViewer/request.pdf?WOPIsrc=http%3A%2F%2Fpsg3-view-wopi%2Ewopi%2Eonline%2Eoffice%2Enet%3A808%2Foh%2Fwopi%2Ffiles%2F%40%2FwFileId%3FwFileId%3D$paramsUrl&access_token=1&access_token_ttl=0&z=dce785126488e4f952cc69b50e330603d7517b89c1f01bd14796eee9b097a030&type=downloadpdf&useNamedAction=1";
+
+        // old
+        // $link = "https://psg4-word-view.officeapps.live.com/wv/WordViewer/request.pdf?WOPIsrc=http%3A%2F%2Fpsg3-view-wopi%2Ewopi%2Eonline%2Eoffice%2Enet%3A808%2Foh%2Fwopi%2Ffiles%2F%40%2FwFileId%3FwFileId%3D$paramsUrl&access_token=1&access_token_ttl=0&z=dce785126488e4f952cc69b50e330603d7517b89c1f01bd14796eee9b097a030&type=downloadpdf&useNamedAction=1";
+        
+        $link = 'https://psg4-word-view.officeapps.live.com/wv/WordViewer/request.pdf?WOPIsrc=http%3A%2F%2Fpsg4-view-wopi%2Ewopi%2Eonline%2Eoffice%2Enet%3A808%2Foh%2Fwopi%2Ffiles%2F%40%2FwFileId%3FwFileId%3D'.$paramsUrl.'&access_token=1&access_token_ttl=0&z=d733b55ce52224bcf73f9681484913a98c5c99fb1ceaf421e8b2e94222556c9f&type=downloadpdf&useNamedAction=1';
         $file = file_get_contents($link);
+
+        // testing
         // $file = convertWordToPdf($paramsUrl);
 
         $name = 'new-result.pdf';
