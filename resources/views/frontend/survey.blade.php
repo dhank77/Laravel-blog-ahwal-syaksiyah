87c6c95b3fa603762da4c5b6df2beb0914b9bbe6
Hamdani Ilham <d41113512@gmail.com>
2024-10-17 15:46:29 +0800

    Update survey.blade.php

--- resources/views/frontend/survey.blade.php
+++ resources/views/frontend/survey.blade.php
@@ -7,7 +7,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <h1>Form Survey</h1>
-                        <p>Survei Tingkat Pemahaman Sivitas Akademika PS Hukum Keluarga (Ahwal Syakhshiyah) Terhadap Visi Keilmuan, Tujuan, dan Strategi Pencapaian Tujuan PS Tahun 2022/2023.</p>
+                        <p>Survei Tingkat Pemahaman Sivitas Akademika PS Hukum Keluarga (Ahwal Syakhshiyah) Terhadap Visi Keilmuan, Tujuan, dan Strategi Pencapaian Tujuan PS Tahun {{ date("Y")-1 }}/{{ date("Y") }}.</p>
                         <form id="contact-form" method="POST" action="{{ route("survey.store") }}">
                             @csrf
                             <div class="row">
@@ -48,31 +48,31 @@
                                     <label>{!! $soal->soal !!}</label>
                                     @if($soal->pilihan1 != "")
                                         <div style="margin-top: -20px; margin-left:20px;">
-                                            <input type="radio" name="{{ $pilihan }}" id="pilihan1" value="{{ $soal->pilihan1 }}"> <label for="pilihan1">{{ $soal->pilihan1 }}</label>
+                                            <input type="radio" name="{{ $pilihan }}" id="pilihan1{{ $nilai }}" value="{{ $soal->pilihan1 }}"> <label for="pilihan1{{ $nilai }}">{{ $soal->pilihan1 }}</label>
                                         </div>
                                     @endif
 
                                     @if($soal->pilihan2 != "")
                                         <div style="margin-left:20px;">
-                                            <input type="radio" name="{{ $pilihan }}" id="pilihan2" value="{{ $soal->pilihan2 }}"> <label for="pilihan2">{{ $soal->pilihan2 }}</label>
+                                            <input type="radio" name="{{ $pilihan }}" id="pilihan2{{ $nilai }}" value="{{ $soal->pilihan2 }}"> <label for="pilihan2{{ $nilai }}">{{ $soal->pilihan2 }}</label>
                                         </div>
                                     @endif
 
                                     @if($soal->pilihan3 != "")
                                         <div style="margin-left:20px;">
-                                            <input type="radio" name="{{ $pilihan }}" id="pilihan3" value="{{ $soal->pilihan3 }}"> <label for="pilihan3">{{ $soal->pilihan3 }}</label>
+                                            <input type="radio" name="{{ $pilihan }}" id="pilihan3{{ $nilai }}" value="{{ $soal->pilihan3 }}"> <label for="pilihan3{{ $nilai }}">{{ $soal->pilihan3 }}</label>
                                         </div>
                                     @endif
 
                                     @if($soal->pilihan4 != "")
                                         <div style="margin-left:20px;">
-                                            <input type="radio" name="{{ $pilihan }}" id="pilihan4" value="{{ $soal->pilihan4 }}"> <label for="pilihan4">{{ $soal->pilihan4 }}</label>
+                                            <input type="radio" name="{{ $pilihan }}" id="pilihan4{{ $nilai }}" value="{{ $soal->pilihan4 }}"> <label for="pilihan4{{ $nilai }}">{{ $soal->pilihan4 }}</label>
                                         </div>
                                     @endif
 
                                     @if($soal->pilihan5 != "")
                                         <div style="margin-left:20px;">
-                                            <input type="radio" name="{{ $pilihan }}" id="pilihan5" value="{{ $soal->pilihan5 }}"> <label for="pilihan5">{{ $soal->pilihan5 }}</label>
+                                            <input type="radio" name="{{ $pilihan }}" id="pilihan5{{ $nilai }}" value="{{ $soal->pilihan5 }}"> <label for="pilihan5{{ $nilai }}">{{ $soal->pilihan5 }}</label>
                                         </div>
                                     @endif
                                 </div>
@@ -84,7 +84,7 @@
                                         placeholder="Validasi Captcha">
                                 </div>
                             </div>
-                            <button type="submit">Kirim Komplain</button>
+                            <button type="submit">Kirim Survey</button>
                         </form>
                     </div>
                 </div>
